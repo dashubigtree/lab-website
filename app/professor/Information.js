6ee3582a77713b2108d138cfec6859b211262d27
dashubigtree <bensonhsu917@gmail.com>
2026-07-08 14:09:58 +0800

    移除教授課程區塊

--- app/professor/Information.js
+++ app/professor/Information.js
@@ -1,9 +1,8 @@
 'use client';
 import * as React from 'react';
 import Image from 'next/image'
-import PropTypes from 'prop-types';
 
-import { Box, Typography, ListItem, Stack, Divider, Grid, Tabs, Tab, Link } from '@mui/material';
+import { Box, Typography, ListItem, Stack, Divider, Grid } from '@mui/material';
 
 import ViewStreamIcon from '@mui/icons-material/ViewStream';
 import PersonPinCircleOutlinedIcon from '@mui/icons-material/PersonPinCircleOutlined';
@@ -89,86 +88,7 @@ const Experience = ({ workplace, position, duringtime }) => (
     </Stack >
 )
 
-const MBACourses = [
-    { course: '數位金融概論111-1', link: 'https://moodle2.ntust.edu.tw/course/view.php?id=4405', img: 'digital-finance' }
-]
-const UniversityCourses = [
-    { course: '計算機概論 112-1', link: 'https://moodle2.ntust.edu.tw/course/view.php?id=8509', img: 'computer-science' },
-    { course: '數位金融概論 112-1', link: 'https://moodle2.ntust.edu.tw/course/view.php?id=8518', img: 'fintech' },
-    { course: '大數據分析導論 111-1', link: 'https://moodle2.ntust.edu.tw/course/view.php?id=4005', img: 'big-data' },
-    { course: '大數據分析導論 110-2', link: 'https://moodle2.ntust.edu.tw/course/view.php?id=1790', img: 'big-data' },
-    { course: '計算機概論 111-2', link: 'https://moodle2.ntust.edu.tw/course/view.php?id=5832', img: 'computer-science' }
-]
-const CourseGrid = ({ course, link, img }) => (
-    <Stack direction="column" justifyContent="center" alignItems="center"
-        spacing={2}
-        component={Link}
-        href={link}
-        target="_blank"
-        sx={{
-            textDecoration: 'none',
-            color: 'inherit',
-            bgcolor: 'white',
-            height: { xs: 200, sm: 250 },
-            transition: "transform 200ms ease-in-out",
-            "&:hover": {
-                cursor: "pointer",
-                boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
-                backgroundColor: "white",
-                transform: "scale(1.02)"
-            }
-        }}>
-        <Image
-            src={`/fields/${img}.svg`}
-            alt={img}
-            width={100}
-            height={100}
-        />
-        <Typography variant='body1' align="center" sx={{ fontWeight: 600 }}>
-            {course}
-        </Typography>
-    </Stack>
-)
-function CustomTabPanel(props) {
-    const { children, value, index, ...other } = props;
-
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`simple-tabpanel-${index}`}
-            aria-labelledby={`simple-tab-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Grid container spacing={{ xs: 2, md: 3, lg: 4 }} columns={{ xs: 4, sm: 9, md: 12, lg: 16 }} >
-                    {children}
-                </Grid>
-            )}
-        </div>
-    );
-}
-
-CustomTabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.number.isRequired,
-    value: PropTypes.number.isRequired,
-};
-
-function a11yProps(index) {
-    return {
-        id: `simple-tab-${index}`,
-        'aria-controls': `simple-tabpanel-${index}`,
-    };
-}
-
 export default function Information() {
-    const [value, setValue] = React.useState(0);
-
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
-
     return (
         <Box sx={{ height: '100%' }}>
 
@@ -223,38 +143,6 @@ export default function Information() {
 
                 </Grid>
             </Grid>
-
-
-            <SubTitle title="教授課程">
-                <Box sx={{ borderBottom: 1, borderColor: 'divider', mb: 3 }}>
-                    <Tabs
-                        value={value}
-                        onChange={handleChange}
-                        aria-label="basic tabs example"
-                        textColor='secondary'
-                        indicatorColor='secondary'
-                        centered
-                    >
-                        <Tab label="MBA" disableRipple {...a11yProps(0)} />
-                        <Tab label="大學部" disableRipple {...a11yProps(1)} />
-                    </Tabs>
-                </Box>
-
-                <CustomTabPanel value={value} index={0}>
-                    {MBACourses.map((mba, index) => (
-                        <Grid size={{ xs: 4, sm: 3, md: 4, lg: 4 }} key={index}>
-                            <CourseGrid course={mba.course} link={mba.link} img={mba.img} />
-                        </Grid>
-                    ))}
-                </CustomTabPanel>
-                <CustomTabPanel value={value} index={1}>
-                    {UniversityCourses.map((university, index) => (
-                        <Grid size={{ xs: 4, sm: 3, md: 4, lg: 4 }} key={index}>
-                            <CourseGrid course={university.course} link={university.link} img={university.img} />
-                        </Grid>
-                    ))}
-                </CustomTabPanel>
-            </SubTitle>
         </Box>
     );
 }
